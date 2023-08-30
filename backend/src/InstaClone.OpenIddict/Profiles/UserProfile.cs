ba5f022bdd957eb9f5437a692fa6bebca3c109f1
Romero <romero.serhio1@gmail.com>
2023-08-30 14:32:53 +0300

    feat: added profile for user

---
+++ backend/src/InstaClone.OpenIddict/Profiles/UserProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using InstaClone.GeneralDto.User;
+using InstaClone.OpenIddict.Data;
+
+namespace InstaClone.OpenIddict.Profiles;
+
+public class UserProfile : Profile
+{
+    public UserProfile()
+    {
+        CreateMap<ApplicationUser, UserDto>().ReverseMap();
+    }
+}
