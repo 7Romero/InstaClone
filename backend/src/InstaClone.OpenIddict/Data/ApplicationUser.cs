fc0300e8a35670ba0627da1c75ad05735d5867b1
Romero <romero.serhio1@gmail.com>
2023-08-30 14:30:45 +0300

    feat: added new fileds for ApplicationUser

--- backend/src/InstaClone.OpenIddict/Data/ApplicationUser.cs
+++ backend/src/InstaClone.OpenIddict/Data/ApplicationUser.cs
@@ -1,6 +1,12 @@
 using Microsoft.AspNetCore.Identity;
+using System;
 
 namespace InstaClone.OpenIddict.Data;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
-public class ApplicationUser : IdentityUser { }
+public class ApplicationUser : IdentityUser<Guid>
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string ProfilePicture { get; set; }
+}
